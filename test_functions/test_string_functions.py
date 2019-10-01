efdd30e96f93527ccfb7e6b8e00a697ff826aec1
Kellymsmith7 <kellymsmith7@gmail.com>
2019-09-30 19:54:28 -0500

    Failed Commit String Function

---
+++ test_functions/test_string_functions.py
@@ -0,0 +1,11 @@
+import unittest
+from string_functions import multiply_string
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(glueglueglueglueglueglueglue, string_functions.multiply_string(7))
+
+
+if __name__ == '__main__':
+    unittest.main()
