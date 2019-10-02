6f2feaa90da86002767a05733adf526cb52d37f7
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-01 21:34:29 -0500

    Second Inner Functions

--- more_functions/inner_functions_assignment.py
+++ more_functions/inner_functions_assignment.py
@@ -3,22 +3,27 @@ Program: inner_functions_assignment
 Author: Kelly Smith
 Last date updated: 10/01/19
 
-Program to output the area and perimeter of a rectangle
+Program to output the area and perimeter of a rectangle or square
 """
 
 
 def measurements():
-    side_one = float(input('How long is your rectangle '))
-    side_two = float(input('How tall is your rectangle '))
-    a_list = [side_one, side_two]
+    a_list = [3.5]
 
     def area():
         a_list
-        print('Area = ', side_one * side_two)
+        if len(a_list) == 2:
+            print('Area = ', a_list[0] * a_list[1])
+        else:
+            print('Area = ', a_list[0] * a_list[0])
 
     def perimeter():
         a_list
-        print('Perimeter = ', (side_one * 2) + (side_two * 2))
+        if len(a_list) == 2:
+            print('Perimeter = ', (a_list[0] * 2) + (a_list[1] * 2))
+        else:
+            print('Perimeter = ', a_list[0] * 4)
+
     area()
     perimeter()
 
