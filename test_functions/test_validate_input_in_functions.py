5eb04409fad177fa0b10ce74ba393d88555081b1
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-01 22:41:54 -0500

    Failed Test Score Input

---
+++ test_functions/test_validate_input_in_functions.py
@@ -0,0 +1,23 @@
+import unittest
+from more_functions import validate_input_in_functions
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual('Math', score_input.validate_input_in_functions, 'Math')
+
+    def test_score_valid(self):
+        self.assertEqual(20, score_input.validate_input_in_functions, 20)
+
+    def test_score_below_range(self):
+        self.assertEqual(-1, score_input.validate_input_in_functions, 'Invalid test score, try again!')
+
+    def test_score_above_range(self):
+        self.assertEqual(120, score_input.validate_input_in_functions, 'Invalid test score, try again!')
+
+    def test_test_score_non_numeric(self):
+        self.assertEqual(a, score_input.validate_input_in_functions, ValueError)
+
+
+if __name__ == '__main__':
+    unittest.main()
