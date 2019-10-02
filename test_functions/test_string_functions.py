a8c7d26b9f6dac86d41013252e29aa5f1470fb28
Kellymsmith7 <kellymsmith7@gmail.com>
2019-10-01 20:41:02 -0500

    TestString Function

--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -5,7 +5,7 @@ from topic3.string_functions import multiply_string
 
 class MyTestCase(unittest.TestCase):
     def test_word_glue(self):
-        self.assertEqual(multiply_string.string_functions)
+        self.assertEqual('glueglue', multiply_string.string_functions, ('glue', 2))
 
 
 if __name__ == '__main__':
